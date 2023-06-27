ef92fb79ce58cd2c4e24729f576198cc0b8cf7f3
Marcos F. Alves <31605679+marcosfalves@users.noreply.github.com>
2023-06-27 08:59:02 -0300

    ci: :construction_worker: Add sonar cloud (#7)

--- src/math_test.go
+++ src/math_test.go
@@ -2,12 +2,14 @@ package main
 
 import "testing"
 
+const RESULT_TEST_MESSAGE = "Result is invalid: Result %d. Expected: %d"
+
 func TestSum(t *testing.T) {
 
 	result := sum(15, 15)
 
 	if result != 30 {
-		t.Errorf("Result is invalid: Result %d. Expected: %d", result, 30)
+		t.Errorf(RESULT_TEST_MESSAGE, result, 30)
 	}
 }
 
@@ -16,7 +18,7 @@ func TestSub(t *testing.T) {
 	result := sub(100, 15)
 
 	if result != 85 {
-		t.Errorf("Result is invalid: Result %d. Expected: %d", result, 85)
+		t.Errorf(RESULT_TEST_MESSAGE, result, 85)
 	}
 }
 
@@ -25,7 +27,7 @@ func TestTimes(t *testing.T) {
 	result := times(10, 5)
 
 	if result != 50 {
-		t.Errorf("Result is invalid: Result %d. Expected: %d", result, 50)
+		t.Errorf(RESULT_TEST_MESSAGE, result, 50)
 	}
 }
 
@@ -34,7 +36,7 @@ func TestSumX(t *testing.T) {
 	result := sumX(10, 5)
 
 	if result != 25 {
-		t.Errorf("Result is invalid: Result %d. Expected: %d", result, 25)
+		t.Errorf(RESULT_TEST_MESSAGE, result, 25)
 	}
 }
 
@@ -43,7 +45,7 @@ func TestSumY(t *testing.T) {
 	result := sumY(10, 5)
 
 	if result != 20 {
-		t.Errorf("Result is invalid: Result %d. Expected: %d", result, 20)
+		t.Errorf(RESULT_TEST_MESSAGE, result, 20)
 	}
 }
 
@@ -52,7 +54,7 @@ func TestDiv(t *testing.T) {
 	result := div(100, 2)
 
 	if result != 50 {
-		t.Errorf("Result is invalid: Result %d. Expected: %d", result, 50)
+		t.Errorf(RESULT_TEST_MESSAGE, result, 50)
 	}
 }
 
@@ -61,6 +63,6 @@ func TestAvg(t *testing.T) {
 	result := avg(50, 75, 100)
 
 	if result != 75 {
-		t.Errorf("Result is invalid: Result %d. Expected: %d", result, 75)
+		t.Errorf(RESULT_TEST_MESSAGE, result, 75)
 	}
 }
